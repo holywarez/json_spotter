32e25a574d22efedbc5b4cf274e889ad60451918
Anatoly Lapshin <a.lapshin@3commas.io>
2023-05-17 10:04:54 +0300

    specs: sample books titles case

--- spec/json_spotter/selection_spec.rb
+++ spec/json_spotter/selection_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe JsonSpotter::Selection do
 
     it { is_expected.to be_falsey }
   end
+
+  context 'when json has books' do
+    let(:json) { SampleJsonFactory.books_sample(authors: 2, books: 10) }
+
+    it 'is empty by default' do
+      expect(subject.any?).to be_falsey
+    end
+
+    context 'when book titles are expected' do
+      let(:query) do
+        {
+          authors: [
+            {
+              books: [
+                {
+                  '$select' => {
+                    'book_name' => 'title'
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      end
+
+      it 'renders the list of book_titles' do
+        expect(subject.to_a).to include('book_name' => be).and(have_attributes(size: 20))
+      end
+    end
+  end
 end
--- spec/support/sample_json_factory.rb
+++ spec/support/sample_json_factory.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
-#
+
+require 'json'
+
 class SampleJsonFactory
   def self.books_sample(authors: 2, books: 10)
     JSON.dump(
@@ -9,7 +11,7 @@ class SampleJsonFactory
             name: "Author#{author_index+1} Surname",
             books: books.times.map do |book_index|
               {
-                name: "Book Number #{book_index+1}",
+                title: "Book Number #{book_index+1}",
                 isbn13: "9783161#{book_index.to_s.rjust(6, '0')}"
               }
             end
